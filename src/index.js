f8c0f8874a7ab0b433d009ba1d746a6af70b62c3
KGS <1698415+theKGS@users.noreply.github.com>
2025-10-23 18:37:58 +0200

    Add second project to test list

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import "./styles.css";
 
 const eventDeleteItem = (e) => {};
 
-const proj = new todo('Some task', 'This is the description and it should be, I think, displayed in italics. I make it a bit longer than necessary to see if it wraps around.', new Date(2025, 12, 2));
-proj.children.push(new todo('Build S', 'It is quite a conundrum this one', new Date(2025, 12, 2)));
-proj.children.push(new todo('Remodel P', 'Dunno what P is, really...', new Date(2025, 12, 2)));
-proj.children[0].children.push(new todo('Q', 'W', new Date(2025, 12, 2)));
-proj.children[0].children.push(new todo('Q', 'W', new Date(2025, 12, 2)));
-proj.children[1].minimized = true;
+const proj1 = new todo('Some task', 'This is the description and it should be, I think, displayed in italics. I make it a bit longer than necessary to see if it wraps around.', new Date(2025, 12, 2));
+proj1.children.push(new todo('Build S', 'It is quite a conundrum this one', new Date(2025, 12, 2)));
+proj1.children.push(new todo('Remodel P', 'Dunno what P is, really...', new Date(2025, 12, 2)));
+proj1.children[0].children.push(new todo('Task D', 'W', new Date(2025, 12, 2)));
+proj1.children[0].children.push(new todo('Task E', 'W', new Date(2025, 12, 2)));
+
+const proj2 = new todo('Unknown project', 'I have no idea what this is about...', new Date(2025, 10, 16));
 
 const projectList = [];
-projectList.push(proj);
+projectList.push(proj1);
+projectList.push(proj2);
 
 render(projectList);
